cc1cbcc74eb8f2b140d1712cbe082451aae4914c
zenginerler <yavuzenginerler@gmail.com>
2021-01-01 01:24:05 -0600

    improved jumping and falling

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    
+    private enum State {idle, running, jumping, falling}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+
+
+    // setting variables
+    public float movementSpeed = 5;
+    public float jumpForce = 7;
     /*
     // better control settings
     public float speed = 10;
-    public float jumpForce = 9;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
     */
@@ -20,8 +27,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
-
 
 
     // Update is called once per frame
@@ -34,23 +41,22 @@ public class PlayerController : MonoBehaviour
         // basic player controls
         if (xAxis < 0) 
         {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            anim.SetBool("running", true);
         }
         else if (xAxis > 0)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            anim.SetBool("running", true);
         }
         else
         {
-            anim.SetBool("running", false);
+            
         }
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
         }
 
 
@@ -78,8 +84,35 @@ public class PlayerController : MonoBehaviour
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
         */
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityState()
+    {
+        if (state == State.jumping)
+        {
+            if (rb.velocity.y < .1f){
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground)){
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 3f){
+            //Moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
 
     }
+
     /*
     // required functions to make better controls work 
     private void Walk(Vector2 dir)
